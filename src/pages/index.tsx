7f2ba235817dfa8fb2f56811b5b098017429a8d6
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-03-05 12:46:09 -0300

    Set up login endpoint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,20 @@
-import Head from "next/head";
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import { useEffect, useState } from "react";
+import { parseCookies } from "nookies";
+
+export default function Index() {
+  const [token, setToken] = useState("");
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    const cookies = parseCookies();
+    setToken(cookies.token || "");
+    setUserId(cookies.userId || "");
+  }, []);
+
+  return (
+    <div>
+      <h1>Token: {token}</h1>
+      <h1>User ID: {userId}</h1>
+    </div>
+  );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,26 +1,53 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { setCookie } from "nookies";
+
 export default function Login() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post<{ token: string; userId: string }>(
+        "/api/login",
+        { email, password }
+      );
+      setCookie(null, "token", response.data.token, { path: "/" });
+      setCookie(null, "userId", response.data.userId, { path: "/" });
+      router.push("/");
+    } catch (error: any) {
+      setError(error.response.data.message);
+    }
+  };
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="flex items-center justify-center w-screen h-screen">
       <form
-        action="/send-data-here"
         method="post"
-        className="flex flex-col gap-2 items-center w-min border justify-center p-4 border-gray-600 rounded"
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center justify-center gap-2 p-4 border border-gray-600 rounded w-min"
       >
         <div className="flex flex-col items-start">
-          <label htmlFor="email" className="font-medium cursor-pointer text-sm">
+          <label htmlFor="email" className="text-sm font-medium cursor-pointer">
             Email
           </label>
           <input
             type="email"
             id="email"
             name="email"
-            className="border border-gray-400 px-2 py-1 rounded-sm"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            className="px-2 py-1 border border-gray-400 rounded-sm"
           />
         </div>
         <div className="flex flex-col items-start">
           <label
             htmlFor="password"
-            className="font-medium cursor-pointer text-sm"
+            className="text-sm font-medium cursor-pointer"
           >
             Password
           </label>
@@ -28,12 +55,14 @@ export default function Login() {
             type="password"
             id="password"
             name="password"
-            className="border border-gray-400 px-2 py-1 rounded-sm"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="px-2 py-1 border border-gray-400 rounded-sm"
           />
         </div>
         <button
           type="submit"
-          className="bg-blue-600 text-white px-2 py-1 rounded active:bg-blue-700 cursor-pointer"
+          className="px-2 py-1 text-white bg-blue-600 rounded cursor-pointer active:bg-blue-700"
         >
           Submit
         </button>
